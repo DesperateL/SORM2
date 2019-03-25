0ae6970c1f5fbc9b7d0758655074b32a4f79d9b1
DesperateL <leungjiaqi95@gmail.com>
2019-03-25 10:51:26 +0800

    Query-工厂方法

--- src/com/xiaojiaqi/test/Test.java
+++ src/com/xiaojiaqi/test/Test.java
@@ -18,14 +18,6 @@ public class Test {
         Query q = QueryFactory.createQuery();
 
 
-        String sql2 = "select e.id,e.empname,salary+bonus 'xinshui' ,age,d.dname 'deptName',d.address 'deptAddr' from emp e join dept d on e.deptId=d.id";
-        List<EmpVO> list1 = new MySqlQuery().queryRows(sql2,
-                EmpVO.class,null);
 
-        //System.out.println(list1==null ? "null" : "not null");
-        for(EmpVO empVO:list1){
-            //System.out.println("???????");
-            System.out.println(empVO.getEmpname()+"-"+empVO.getDeptName()+"-"+empVO.getXinshui());
-        }
     }
 }
